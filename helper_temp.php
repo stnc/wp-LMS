241e46fac7b5cd7b8af7ef155a752258112baf09
stnc <samtunc@yahooo.com>
2025-07-27 23:19:50 -0600

    fix

--- helper_temp.php
+++ helper_temp.php
@@ -6,14 +6,14 @@ add_action('init', 'app_output_buffer');
 
 
 function helix_default_setting(){
-    $version = '1.0.5';
+    $version = '2.0.0';
     $helix_plugin_path= trailingslashit(plugin_dir_path(__FILE__));
     $helix_plugin_url= trailingslashit(plugins_url( __FILE__));
     $helix_plugin_dir_url= trailingslashit(plugin_dir_url( __FILE__));
 
      return  array( 
-       "version" =>  $version,
-       "helixPluginPath" =>  $helix_plugin_path, 
+      "version" =>  $version,
+      "helixPluginPath" =>  $helix_plugin_path, 
       "helixPluginUrl" =>   $helix_plugin_url,
       "helixPluginDirUrl" => $helix_plugin_dir_url,
      );
@@ -22,8 +22,6 @@ function helix_default_setting(){
 
 function helix_admin_body_class($classes = '')
 {
-  // $onboarding_class = isset( $_GET['page'] ) && 'helix_homepage' === $_GET['page'] ? 'helix-header-page' : ''; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-  // $classes .= ' ' . $onboarding_class . ' ';
   $onboarding_class = '';
   if (isset($_GET['page']) && 'helix_homepage' === $_GET['page']) {
     $onboarding_class = 'helix-header-page';
@@ -43,6 +41,22 @@ add_action('admin_body_class',  'helix_admin_body_class');
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 add_shortcode( 'cta_button', 'salcodes_cta' );
 
 function salcodes_cta( $atts ) {
@@ -57,6 +71,8 @@ function salcodes_cta( $atts ) {
  $output = '<p><a href="' . esc_url( $a['link'] ) . '" id="' . esc_attr( $a['id'] ) . '" class="button ' . esc_attr( $a['color'] ) . ' ' . esc_attr( $a['size'] ) . '" target="' . esc_attr($a['target']) . '">' . esc_attr( $a['label'] ) . '</a></p>';
  return $output;
 }
+*/
+
 
 /*
  <div class="container 44">
